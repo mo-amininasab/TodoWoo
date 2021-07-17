74347994f75fb93ddd8083db8d95b8139c237dcf
mohammad_amini_nasab <mammadkingall@yahoo.com>
2021-07-17 20:08:18 +0430

    Authenticate added.

--- todo/urls.py
+++ todo/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from .views import  signup_user, current_todos
+from .views import  signup_user, current_todos, logout_user, login_user
 
 app_name = 'todo'
 
 urlpatterns = [
     # Auth
     path('signup/', signup_user.as_view(), name='signup_page'),
+    path('logout/', logout_user.as_view(), name='logout_page'),
+    path('login/', login_user.as_view(), name='login_page'),
 
     # Todos
     path('current/', current_todos, name='current_page'),
