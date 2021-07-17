4193f8dacc5b3ab9af4836a896a91635ef3f62e5
mohammad_amini_nasab <mammadkingall@yahoo.com>
2021-07-17 20:08:24 +0430

    Authenticate added.

--- todo/views.py
+++ todo/views.py
@@ -2,8 +2,12 @@ from django.views import View
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login
+from django.contrib.auth import login, logout, authenticate
 
+def current_todos(request):
+    return render(request, 'todo/currenttodos.html', {})
+
+######################################################## Authenticate
 class signup_user(View):
     def get(self, request):
         return render(request, 'todo/signupuser.html')
@@ -12,7 +16,7 @@ class signup_user(View):
         if request.POST.get('password1') == request.POST.get('password2'):
             try:
                 username = request.POST.get('username')
-                password = request.POST.get('username')
+                password = request.POST.get('password1')
 
                 user = User.objects.create_user(username=username, password=password)
                 login(request, user)
@@ -25,5 +29,29 @@ class signup_user(View):
         else:
             return render(request, 'todo/signupuser.html', {'not_match_error': 'Passwords did not match.'})
 
-def current_todos(request):
-    return render(request, 'todo/currenttodos.html', {})
+
+class logout_user(View):
+    def post(self, request):
+        logout(request)
+        return redirect('/current/')
+
+class login_user(View):
+    def get(self, request):
+        return render(request, 'todo/loginuser.html', {})
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # authenticate, give a user object or None.
+        user = authenticate(request, username=username, password=password)
+
+        if user == None:
+            return render(request, 'todo/loginuser.html', {'not_found_user_error': 'User not found.'})
+
+        else:
+            login(request, user)
+            return redirect('/current/')
+
+########################################################
+
