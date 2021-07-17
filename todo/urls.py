8d250a07ea3b66faa63b7a9ede6313c6cb4d082a
mohammad_amini_nasab <mammadkingall@yahoo.com>
2021-07-17 16:56:59 +0430

    signup page, syiled.

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import  signup_user
+from .views import  signup_user, current_todos
 
 app_name = 'todo'
 
 urlpatterns = [
     # Auth
-    path('signup/', signup_user, name='signup_page'),
+    path('signup/', signup_user.as_view(), name='signup_page'),
+
+    # Todos
+    path('current/', current_todos, name='current_page'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,17 +1,29 @@
-import django
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.views import View
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login
 
-def signup_user(request):
-    if request.method == 'GET':
-        return render(request, 'todo/signupuser.html', {'form': UserCreationForm})
+class signup_user(View):
+    def get(self, request):
+        return render(request, 'todo/signupuser.html')
+    
+    def post(self, request):
+        if request.POST.get('password1') == request.POST.get('password2'):
+            try:
+                username = request.POST.get('username')
+                password = request.POST.get('username')
 
-    elif request.method == 'POST':
-        if request.POST.get('password1') == request.POST.get('password1'):
-            username = request.POST.get('username')
-            password = request.POST.get('username')
-            
-            User.objects.create_user(username=username, password=password)
-           
-            return render(request, 'todo/signupuser.html', {'form': UserCreationForm})
+                user = User.objects.create_user(username=username, password=password)
+                login(request, user)
+
+                return redirect('/current/')
+            except IntegrityError:
+
+                return render(request, 'todo/signupuser.html', {'been_taken_error': 'That username had already been taken.'})
+
+        else:
+            return render(request, 'todo/signupuser.html', {'not_match_error': 'Passwords did not match.'})
+
+def current_todos(request):
+    return render(request, 'todo/currenttodos.html', {})
